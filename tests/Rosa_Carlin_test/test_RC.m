ea60974eb24defc3c41f0f2459b4c9e920a2cb4b
ShouWenWang <wangsw09@gmail.com>
2022-01-21 17:42:06 -0500

    Add new test datasets

---
+++ tests/Rosa_Carlin_test/test_RC.m
@@ -0,0 +1,15 @@
+input_dir=pwd;
+output_dir=input_dir+"/output";
+cd ../.. % Go back to the main directory
+SampleList="RC-DNA-test";
+
+cfg_type="BulkDNA_Rosa_14UMI";
+template='Rosa';
+my_CARLIN_pipeline(SampleList,cfg_type,input_dir,output_dir,template,'read_cutoff_override',1, 'read_cutoff_floor',1)
+% output_all_from_summary(SampleList,output_dir,template)
+%csv_reports(SampleList,output_dir,template)
+
+% SampleList="RC-DNA-test";
+% make_allele_bank(SampleList,output_dir,template)
+
+cd(input_dir)
---
+++ tests/cCarlin_test/test_CC.m
@@ -0,0 +1,15 @@
+input_dir=pwd;
+output_dir=input_dir+"/output";
+cd ../.. % Go back to the main directory
+SampleList="CC-DNA-test";
+
+cfg_type="BulkDNA_12UMI";
+template='cCARLIN';
+my_CARLIN_pipeline(SampleList,cfg_type,input_dir,output_dir,template,'read_cutoff_override',1, 'read_cutoff_floor',1)
+% output_all_from_summary(SampleList,output_dir,template)
+%csv_reports(SampleList,output_dir,template)
+
+% SampleList="CC-DNA-test";
+% make_allele_bank(SampleList,output_dir,template)
+
+cd(input_dir)
