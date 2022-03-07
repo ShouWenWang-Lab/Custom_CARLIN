05d9e864cf86cbed8eaa7679d29aa8a18d92abf0
ShouWenWang <wangsw09@gmail.com>
2022-03-07 16:34:46 -0500

    Add more test samples for low-quality data

--- tests/Rosa_Carlin_test/test_RC.m
+++ tests/Rosa_Carlin_test/test_RC.m
@@ -1,16 +1,37 @@
+% input_dir=pwd;
+% output_dir=input_dir+"/output";
+% cd ../.. % Go back to the main directory
+% SampleList="RC-DNA-test";
+% 
+% cfg_type="BulkDNA_Rosa_14UMI";
+% template='Rosa';
+% my_CARLIN_pipeline(SampleList,cfg_type,input_dir,output_dir,template,'read_cutoff_override',1, 'read_cutoff_floor',1)
+% % output_all_from_summary(SampleList,output_dir,template)
+% %csv_reports(SampleList,output_dir,template)
+% 
+% SampleList="RC-DNA-test";
+% %merge_samples(SampleList,output_dir,template)
+% make_allele_bank(SampleList,output_dir,template)
+% 
+% cd(input_dir)
+
+
+
 input_dir=pwd;
 output_dir=input_dir+"/output";
+SampleList="RC_RNA_poor";
 cd ../.. % Go back to the main directory
-SampleList="RC-DNA-test";
+cfg_type="BulkRNA_Rosa_14UMI";
+template='Rosa';
+my_CARLIN_pipeline(SampleList,cfg_type,input_dir,output_dir,template,'read_cutoff_override',1, 'read_cutoff_floor',1)
+cd(input_dir)
 
+
+input_dir=pwd;
+output_dir=input_dir+"/output";
+SampleList="RC_DNA_poor";
+cd ../.. % Go back to the main directory
 cfg_type="BulkDNA_Rosa_14UMI";
 template='Rosa';
 my_CARLIN_pipeline(SampleList,cfg_type,input_dir,output_dir,template,'read_cutoff_override',1, 'read_cutoff_floor',1)
-% output_all_from_summary(SampleList,output_dir,template)
-%csv_reports(SampleList,output_dir,template)
-
-SampleList="RC-DNA-test";
-%merge_samples(SampleList,output_dir,template)
-make_allele_bank(SampleList,output_dir,template)
-
 cd(input_dir)
--- tests/cCarlin_test/test_CC.m
+++ tests/cCarlin_test/test_CC.m
@@ -1,16 +1,37 @@
+% input_dir=pwd;
+% output_dir=input_dir+"/output";
+% cd ../.. % Go back to the main directory
+% SampleList="CC-DNA-test";
+% 
+% cfg_type="BulkRNA_12UMI";
+% template='cCARLIN';
+% my_CARLIN_pipeline(SampleList,cfg_type,input_dir,output_dir,template,'read_cutoff_override',1, 'read_cutoff_floor',1)
+% %output_selected_from_summary(SampleList,output_dir,template)
+% %csv_reports(SampleList,output_dir,template)
+% 
+% % SampleList="CC-DNA-test";
+% %merge_samples(SampleList,output_dir,template)
+% %make_allele_bank(SampleList,output_dir,template)
+% 
+% cd(input_dir)
+
+
+
 input_dir=pwd;
 output_dir=input_dir+"/output";
+SampleList="CC_RNA_poor";
 cd ../.. % Go back to the main directory
-SampleList="CC-DNA-test";
+cfg_type="BulkRNA_12UMI";
+template='cCARLIN';
+my_CARLIN_pipeline(SampleList,cfg_type,input_dir,output_dir,template,'read_cutoff_override',1, 'read_cutoff_floor',1)
+cd(input_dir)
 
+
+input_dir=pwd;
+output_dir=input_dir+"/output";
+SampleList="CC_DNA_poor";
+cd ../.. % Go back to the main directory
 cfg_type="BulkDNA_12UMI";
 template='cCARLIN';
-%my_CARLIN_pipeline(SampleList,cfg_type,input_dir,output_dir,template,'read_cutoff_override',1, 'read_cutoff_floor',1)
-output_selected_from_summary(SampleList,output_dir,template)
-csv_reports(SampleList,output_dir,template)
-
-% SampleList="CC-DNA-test";
-%merge_samples(SampleList,output_dir,template)
-%make_allele_bank(SampleList,output_dir,template)
-
+my_CARLIN_pipeline(SampleList,cfg_type,input_dir,output_dir,template,'read_cutoff_override',1, 'read_cutoff_floor',1)
 cd(input_dir)
