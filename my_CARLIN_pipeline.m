b3d5556891d139800ed544a0e80a0e2eff24757a
ShouWenWang <wangsw09@gmail.com>
2022-07-19 16:41:31 -0400

    Add CB and UMI cutoff threshold

--- my_CARLIN_pipeline.m
+++ my_CARLIN_pipeline.m
@@ -2,11 +2,11 @@ function my_CARLIN_pipeline(SampleList,cfg_type,input_dir,output_dir,template,va
 
     %% Note
     % we assume that the current dir is where Custom_CARLIN is
-    % currently support variables: 'template', 'read_cutoff_override', 'read_cutoff_floor'
+    % currently support variables: 'template', 'read_cutoff_UMI_override', 'read_cutoff_CB_override'
     p0 = inputParser;
     cur_dir=pwd;
-    p0.addParameter('read_cutoff_override',NaN);
-    p0.addParameter('read_cutoff_floor',10);
+    p0.addParameter('read_cutoff_UMI_override',NaN);
+    p0.addParameter('read_cutoff_CB_override',NaN);
     p0.addParameter('CARLIN_dir',cur_dir);
     p0.parse(varargin{:});
     res=p0.Results;
@@ -35,11 +35,11 @@ function my_CARLIN_pipeline(SampleList,cfg_type,input_dir,output_dir,template,va
         if startsWith(cfg_type,'Bulk')
             sample_dir=sample_name+".trimmed.pear.assembled.fastq";
 
-            analyze_CARLIN(char(sample_dir),char(sample_type), char(output_dir_1),'read_override_UMI_denoised',res.read_cutoff_override,'read_cutoff_UMI_denoised',res.read_cutoff_floor);
+            analyze_CARLIN(char(sample_dir),char(sample_type), char(output_dir_1),'read_override_UMI_denoised',res.read_cutoff_UMI_override);
         elseif startsWith(cfg_type,'scLimeCat') % we reverse R1 and R2 here
             fastq_file = {sample_name+"_L001_R2_001.fastq.gz", sample_name+"_L001_R1_001.fastq.gz"};
-            analyze_CARLIN(fastq_file,char(sample_type), char(output_dir_1),'read_override_CB_denoised',res.read_cutoff_override,'read_cutoff_CB_denoised',res.read_cutoff_floor);
-
+            analyze_CARLIN(fastq_file,char(sample_type), char(output_dir_1), ...
+                'read_override_CB_denoised',res.read_cutoff_CB_override,'read_override_UMI_denoised',res.read_cutoff_UMI_override);
         else
             error('Unsupported scLimeCat version');
         end
