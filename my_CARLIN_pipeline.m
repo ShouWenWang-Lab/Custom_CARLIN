a0b1e23d72992317ce8606c22d2efb12af8fd6b6
ShouWenWang <wangsw09@gmail.com>
2024-11-05 11:25:25 +0800

    Breaking change: expected fastq file changed from L001_R1 tO R1

--- my_CARLIN_pipeline.m
+++ my_CARLIN_pipeline.m
@@ -37,12 +37,12 @@ function my_CARLIN_pipeline(SampleList,cfg_type,input_dir,output_dir,template,va
 
             analyze_CARLIN(char(sample_dir),char(sample_type), char(output_dir_1),'read_override_UMI_denoised',res.read_cutoff_UMI_override);
         elseif startsWith(cfg_type,'scCamellia') % we reverse R1 and R2 here
-            fastq_file = {sample_name+"_L001_R2_001.fastq.gz", sample_name+"_L001_R1_001.fastq.gz"};
+            fastq_file = {sample_name+"_R2.fastq.gz", sample_name+"_R1.fastq.gz"};
             analyze_CARLIN(fastq_file,char(sample_type), char(output_dir_1), ...
                 'read_override_CB_denoised',res.read_cutoff_CB_override,'read_override_UMI_denoised',res.read_cutoff_UMI_override);
         else
             % uinsg 10X etc
-            fastq_file = {sample_name+"_L001_R1_001.fastq.gz", sample_name+"_L001_R2_001.fastq.gz"};
+            fastq_file = {sample_name+"_R1.fastq.gz", sample_name+"_R2.fastq.gz"};
             analyze_CARLIN(fastq_file,char(sample_type), char(output_dir_1), ...
                 'read_override_CB_denoised',res.read_cutoff_CB_override,'read_override_UMI_denoised',res.read_cutoff_UMI_override);
         end
